1d38b08d0df8c817319d768f738522e1de67fede
Khai PHAM (Mr.Kay) <khaipham2204@gmail.com>
2024-09-07 16:47:05 +0700

    Update main.py

--- Data Types/main.py
+++ Data Types/main.py
@@ -1,1 +1,10 @@
-
+'''
+  Data Types
+  |_ Primitive
+    |_ int, float, str, boolean
+  |_ Non-Primitive
+    |_ Linear
+      |_ Static: array
+      |_ Dynamic: linked-list, stack, queue
+    |_ Non-Linear: tree, graph
+'''
